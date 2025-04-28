38f39fb3dfa1c150b53169c8cff52679fb242cb8
patrick <patrickw4500@gmail.com>
2025-04-28 02:31:14 -0400

    General Comment & Requirement Cleanup

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -14,79 +14,21 @@ NEWSPIDER_MODULE = "wiki.spiders"
 
 LOG_LEVEL = 'INFO'
 
-
+# Spoofed User Agent
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
 
-# Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 2
-
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 10
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "wiki.middlewares.WikiSpiderMiddleware": 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "wiki.middlewares.WikiDownloaderMiddleware": 543,
-#}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "wiki.pipelines.WikiPipeline": 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+# All of the settings below are set in order to be very 'gentle' on the hosting server
+# and so as not to get noticed by cloudflare. In particular, AUTOTHROTTLE_TARGET_CONCURRENCY
+# is half of what the scrapy docs consider a 'gentle' value (0.50).
+# https://docs.scrapy.org/en/latest/topics/autothrottle.html#autothrottle-target-concurrency
 AUTOTHROTTLE_ENABLED = True
-# The initial download delay
 AUTOTHROTTLE_START_DELAY = 10
-# The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
 AUTOTHROTTLE_TARGET_CONCURRENCY = 0.25
-# Enable showing throttling stats for every response received:
+# Enable showing throttling stats for every response received if necessary:
 #AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
@@ -96,10 +38,17 @@ DOWNLOAD_HANDLERS =  {
     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 }
-
+# For whatever reason, chrome does not work but 'chromium does', scrapy stating that it did not find 'chrome' in
+# a specific channel.
 PLAYWRIGHT_BROWSER_TYPE =  "chromium"
 PLAYWRIGHT_LAUNCH_OPTIONS = {
+    # Most headless clients are easily detected, hence, we use our scraper in a virtual display to effectively make a
+    # headful client headless via xfvb.
     "headless" : False,
+    # The disabled automation Chrome flag will lower our chances of being detected as headless.
+    # The latter I'm not so sure how per se it does lower our score from 38% headless-like to 31%,
+    # based off of the following website which is meant to benchmark our client's resilience against bot detection:
+    # https://abrahamjuliot.github.io/creepjs/
     "args" : [
         "--disable-blink-features=AutomationControlled",
         "--enable-experimental-web-platform-features",
--- wiki/wiki/spiders/wiki_spider.py
+++ wiki/wiki/spiders/wiki_spider.py
@@ -1,17 +1,10 @@
-import time
-from pathlib import Path
-
 import scrapy
-from numpy.ma.core import less_equal
 from scrapy.spiders import Rule, CrawlSpider
 from scrapy.linkextractors import LinkExtractor
 
 
 class WikiSpider(CrawlSpider):
     name = "wiki_spider"
-    #"https://www.scrapingcourse.com/cloudflare-challenge/",
-    #"https://abrahamjuliot.github.io/creepjs/",
-
     allowed_domains = ["en.touhouwiki.net", "www.thpatch.net"]
     start_urls = [
         'https://en.touhouwiki.net/wiki/Embodiment_of_Scarlet_Devil/Story',
@@ -55,19 +48,18 @@ class WikiSpider(CrawlSpider):
              process_request=modify_request),
     )
 
-    # Change to async if needing to debug with playwright
     def parse_item(self, response):
-        # BRs are not transferred over into text. Hence, they must be replaced for our purposes.
-        # See https://stackoverflow.com/a/8748836
+        # All the tables that contain dialogue all have cellpadding set to 5.
         for table in response.xpath("//table[@cellpadding = '5']"):
-                # Explanation: because our table's rows alternate between th,tr,tr and tr, tr, tr
-                # we need an agnostic solution. The below will select from the tables on the page,
-                # and then use the child selector (/*) to select each respective part of the row.
-                # tbody -> tr -> columns of tr
-
+                # The below will select from the tables on the page and then use the child selector (/*)
+                # to select each respective row (i.e. table-> tbody -> tr)
                 for row in table.xpath('./*/*'):
                     domain = response.url.split("//")[1].split("/")[0]
 
+                    # Scenarios and Extra Stories are stored under the "en.touhouwiki.net" domain,
+                    # Endings in the 'www.thpatch.net' directory.
+                    # Normally, we would put an elif to ensure we are only on 'www.thpatch.net', but as I understand it,
+                    # allowed_domains takes care of this for us already.
                     if domain == "en.touhouwiki.net":
                         game = response.request.url.split('/')[-3]
                         route = response.request.url.split('/')[-1].split('%27s_')[0]
@@ -80,33 +72,26 @@ class WikiSpider(CrawlSpider):
                     # Example of the URL : https://en.touhouwiki.net/wiki/Perfect_Cherry_Blossom/Story/Reimu%27s_Scenario
                     # Generally holds true for all pages to my knowledge, with the following exception of one game:
                     # https://en.touhouwiki.net/wiki/Wily_Beast_and_Weakest_Creature/Story/Reimu%27s_Scenario_(Wolf)
+                    # in which case we need to edit our extraction method to get the correct route/scenario.
                     if game== "Wily_Beast_and_Weakest_Creature":
                         route += " " + response.request.url.split('_')[-1]
                         if not "Ending" in act:
                             act = act.split('_')[0]
                     yield {
-                        # Get all text from the specific column of the row, whilst discarding extra newlines.
-                        #'character': ''.join(row.xpath('./*[1]//text()').extract()),
-                        #'jp_dialogue': ''.join(row.xpath('./*[2]//text()').extract()),
-                        #'en_dialogue': ''.join(row.xpath('./*[3]//text()').extract()),
-                        'character': ''.join("\n" if text == "<br>" else text for text in row.xpath('./*[1]//text()[not(ancestor::code)] | ./*[1]//br').extract()),
+                        # Many niceties in extraction:
+                        # 1. Because the elements of each row alternate between any combination of td/td/td or th/td/td,
+                        # we need to use the xpath selector agnostic of a single element.
+                        # 2. //text() will extract text in a staggered format via a list, hence we need to combine the
+                        # extracted texts into a single string.
+                        # 3. BRs are not natively rendered as newlines in our default xpath, hence, we need to manually
+                        # extract them and change them to be as such. This will make sanitization easier for us later.
+                        # 4. For whatever reason, the 'Endings' section is broken and contains garbage text enclosed
+                        # within the 'code' HTML element (i.e. <code> </code>)
+                        'character': ''.join("\n" if text == "<br>" else text for text in row.xpath('./*[1]// text()[not(ancestor::code)] | ./*[1]//br').extract()),
                         'jp_dialogue': ''.join("\n" if text == "<br>" else text for text in row.xpath('./*[2]//text()[not(ancestor::code)] | ./*[2]//br').extract()),
                         'en_dialogue': ''.join("\n" if text == "<br>" else text for text in row.xpath('./*[3]//text()[not(ancestor::code)] | ./*[3]//br').extract()),
                         'route': route,
                         'act': act,
                         'game': game
-
                     }
-    # page = response.meta['playwright_page']
-        # yield await page.screenshot(path=f'example.png')
-        # yield Path("quotes-1.html").write_bytes(response.body)
-
-    """
-    Rule(LinkExtractor(allow=r"^.*%27s_Scenario.*$"), callback='parse_item', follow=False,
-             process_request=modify_request),
-    Rule(LinkExtractor(allow=r"^.*%27s_Endings.*$"), callback='parse_item', follow=False,
-         process_request=modify_request),
-    Rule(LinkExtractor(allow=r"^.*%27s_Extra.*$"), callback = 'parse_item', follow = False,
-process_request = modify_request),
-    """
 
